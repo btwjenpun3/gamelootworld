52d2949bc61381afab842bb393ca6ecac8309669
Helmi <muhamadkelmi@gmail.com>
2023-09-27 09:41:13 +0700

    Update Command

--- app/Telegram/Commands/UpdateCommand.php
+++ app/Telegram/Commands/UpdateCommand.php
@@ -3,6 +3,8 @@
 namespace App\Telegram\Commands;
 
 use Telegram\Bot\Commands\Command;
+use App\Http\Controllers\Fetch\FetchController;
+use App\Models\FetchStatus;
 
 class UpdateCommand extends Command
 {
@@ -11,8 +13,12 @@ class UpdateCommand extends Command
 
     public function handle()
     {
+        $update = new FetchController();
+        $response = $update->updateGameContentFromUpstream();
+        $data = json_decode($response, true);
+
         $this->replyWithMessage([
-            'text' => 'Hey, there! Welcome to our bot!',
+            'text' => $data,
         ]);
     }
 }
