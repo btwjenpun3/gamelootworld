1b13874c64f2df0b0a8a16daf90da57c6f2f7310
Helmi <muhamadkelmi@gmail.com>
2023-09-27 09:28:26 +0700

    Update Webhook

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -17,5 +17,14 @@ class TelegramController extends Controller
 
     public function commandHandlerWebHook() {
         $updates = Telegram::commandsHandler(true);
+        $chat_id = $updates->getChat()->getId();
+        $username = $updates->getChat()->getFirstName();
+
+        if(strtolower($updates->getMessage()->getText() === 'halo')) {
+            return Telegram::sendMessage([
+                'chat_id' => $chat_id,
+                'text' => 'Halo'.$username 
+            ]);
+        }
     }
 }
