25fe4882405c5c69c24c189ffa7ecc876b47a062
Helmi <muhamadkelmi@gmail.com>
2023-10-01 07:35:55 +0700

    Fix JS & CSS Homepage

--- resources/views/Layouts/Home/home.blade.php
+++ resources/views/Layouts/Home/home.blade.php
@@ -18,10 +18,10 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="/css/font-awesome.min.css" type="text/css">
     <link rel="stylesheet" href="/css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="/css/plyr.css" type="text/css">
+    {{-- <link rel="stylesheet" href="/css/plyr.css" type="text/css">
     <link rel="stylesheet" href="/css/nice-select.css" type="text/css">
     <link rel="stylesheet" href="/css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="/css/slicknav.min.css" type="text/css"> --}}
     <link rel="stylesheet" href="/css/style.css" type="text/css">
 </head>
 
@@ -62,14 +62,11 @@
     <script src="/js/jquery-3.3.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/player.js"></script>
+    {{-- <script src="/js/player.js"></script>
     <script src="/js/jquery.nice-select.min.js"></script>
     <script src="/js/mixitup.min.js"></script>
     <script src="/js/jquery.slicknav.js"></script>
-    <script src="/js/owl.carousel.min.js"></script>
+    <script src="/js/owl.carousel.min.js"></script> --}}
     <script src="/js/main.js"></script>
-
-
 </body>
-
 </html>
